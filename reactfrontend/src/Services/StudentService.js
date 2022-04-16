98b32501a7a0ce80db6315520367f102e3211c80
kunalagrawal4 <kunalagrawal421@gmail.com>
2022-04-16 11:43:52 +0530

    changes in api

--- reactfrontend/src/Services/StudentService.js
+++ reactfrontend/src/Services/StudentService.js
@@ -1,28 +1,26 @@
-import axios from 'axios';
-const Student_BASE_REST_APT_URL= #yourAPIhello hhh;
+import axios from "axios";
+const Student_BASE_REST_APT_URL = "http://localhost:8080/yourapi";
 
 class StudentService {
-    getAllStudents(){
-        return axios.get(Student_BASE_REST_APT_URL)
-    }
+  getAllStudents() {
+    return axios.get(Student_BASE_REST_APT_URL);
+  }
 
-    addStudent(student){
-        return axios.post(Student_BASE_REST_APT_URL,student)
+  addStudent(student) {
+    return axios.post(Student_BASE_REST_APT_URL, student);
+  }
 
+  getStudentId(studentId) {
+    return axios.get(Student_BASE_REST_APT_URL + "/" + studentId);
+  }
 
-    }
+  updateStudent(studentId, student) {
+    return axios.put(Student_BASE_REST_APT_URL + "/" + studentId, student);
+  }
 
-    getStudentId(studentId){
-        return axios.get(Student_BASE_REST_APT_URL+'/'+studentId);
-    }
-
-    updateStudent(studentId,student){
-        return axios.put(Student_BASE_REST_APT_URL+'/'+studentId,student)
-    }
-
-    deleteStudent(studentId){
-        return axios.delete(Student_BASE_REST_APT_URL+'/'+studentId)
-    }
+  deleteStudent(studentId) {
+    return axios.delete(Student_BASE_REST_APT_URL + "/" + studentId);
+  }
 }
 
 export default new StudentService();
